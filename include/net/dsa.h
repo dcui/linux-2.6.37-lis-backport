46d381896709b2a0feac7d6b9204c69aace0c891
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2024-12-10 14:18:16 +0000

    net: dsa: add hook to determine whether EEE is supported
    BugLink: https://bugs.launchpad.net/bugs/2138502
    
    commit 9723a77318b7c0cfd06ea207e52a042f8c815318 upstream.
    
    Add a hook to determine whether the switch supports EEE. This will
    return false if the switch does not, or true if it does. If the
    method is not implemented, we assume (currently) that the switch
    supports EEE.
    
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Reviewed-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Reviewed-by: Vladimir Oltean <olteanv@gmail.com>
    Link: https://patch.msgid.link/E1tL144-006cZD-El@rmk-PC.armlinux.org.uk
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Edoardo Canepa <edoardo.canepa@canonical.com>

--- include/net/dsa.h
+++ include/net/dsa.h
@@ -990,6 +990,7 @@ struct dsa_switch_ops {
 	/*
 	 * Port's MAC EEE settings
 	 */
+	bool	(*support_eee)(struct dsa_switch *ds, int port);
 	int	(*set_mac_eee)(struct dsa_switch *ds, int port,
 			       struct ethtool_eee *e);
 	int	(*get_mac_eee)(struct dsa_switch *ds, int port,
