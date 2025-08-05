2ffe1f32a3ebb2326f3b488070b8723c49626aeb
Konstantin Taranov <kotaranov@microsoft.com>
2025-08-05 16:07:39 -0500

    RDMA/mana_ib: Set correct device into ib
    BugLink: https://bugs.launchpad.net/bugs/2119572
    
    Add mana_get_primary_netdev_rcu helper to get a primary
    netdevice for a given port. When mana is used with
    netvsc, the VF netdev is controlled by an upper netvsc
    device. In a baremetal case, the VF netdev is the
    primary device.
    
    Use the mana_get_primary_netdev_rcu() helper in the mana_ib
    to get the correct device for querying network states.
    
    Fixes: 8b184e4f1c32 ("RDMA/mana_ib: Enable RoCE on port 1")
    Signed-off-by: Konstantin Taranov <kotaranov@microsoft.com>
    Link: https://lore.kernel.org/r/1720705077-322-1-git-send-email-kotaranov@linux.microsoft.com
    Reviewed-by: Long Li <longli@microsoft.com>
    Reviewed-by: Zhu Yanjun <yanjun.zhu@linux.dev>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    (backported from commit 1df03a4b44146c4f720d793915747272c7773a3e)
    [john-cabaj: not including context from 8859f009ace2:
    "RDMA/mana_ib: Configure mac address in RNIC" as it's not
    relevant to this commit]
    Signed-off-by: John Cabaj <john.cabaj@canonical.com>
    Acked-by: Thibault Ferrante <thibault.ferrante@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off-by: John Cabaj <john.cabaj@canonical.com>

--- drivers/infiniband/hw/mana/device.c
+++ drivers/infiniband/hw/mana/device.c
@@ -51,7 +51,7 @@ static int mana_ib_probe(struct auxiliary_device *adev,
 {
 	struct mana_adev *madev = container_of(adev, struct mana_adev, adev);
 	struct gdma_dev *mdev = madev->mdev;
-	struct net_device *upper_ndev;
+	struct net_device *ndev;
 	struct mana_context *mc;
 	struct mana_ib_dev *dev;
 	int ret;
@@ -78,15 +78,15 @@ static int mana_ib_probe(struct auxiliary_device *adev,
 	dev->ib_dev.num_comp_vectors = 1;
 	dev->ib_dev.dev.parent = mdev->gdma_context->dev;
 
-	rcu_read_lock(); /* required to get upper dev */
-	upper_ndev = netdev_master_upper_dev_get_rcu(mc->ports[0]);
-	if (!upper_ndev) {
+	rcu_read_lock(); /* required to get primary netdev */
+	ndev = mana_get_primary_netdev_rcu(mc, 0);
+	if (!ndev) {
 		rcu_read_unlock();
 		ret = -ENODEV;
-		ibdev_err(&dev->ib_dev, "Failed to get master netdev");
+		ibdev_err(&dev->ib_dev, "Failed to get netdev for IB port 1");
 		goto free_ib_device;
 	}
-	ret = ib_device_set_netdev(&dev->ib_dev, upper_ndev, 1);
+	ret = ib_device_set_netdev(&dev->ib_dev, ndev, 1);
 	rcu_read_unlock();
 	if (ret) {
 		ibdev_err(&dev->ib_dev, "Failed to set ib netdev, ret %d", ret);
