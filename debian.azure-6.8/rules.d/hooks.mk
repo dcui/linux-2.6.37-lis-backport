53ac799e06dfa53eed3d456b37ad0aac9df3b61d
John Cabaj <john.cabaj@canonical.com>
2025-10-03 14:09:09 -0500

    UBUNTU: Start new release
    Ignore: yes
    Signed-off-by: John Cabaj <john.cabaj@canonical.com>

--- debian.azure-6.8/rules.d/hooks.mk
+++ debian.azure-6.8/rules.d/hooks.mk
@@ -11,3 +11,7 @@ do_tools_common     = false
 do_tools_host       = false
 do_lib_rust     = false
 gcc = gcc-11
+do_tools_common     = false
+do_tools_host       = false
+do_lib_rust     = false
+gcc = gcc-11
