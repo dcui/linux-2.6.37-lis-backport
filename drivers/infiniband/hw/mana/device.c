intel-lab-lkp/linux

RDMA/mana_ib: Configure mac address in RNIC
Set local mac address in RNIC, which is required by the HW.

Signed-off-by: Konstantin Taranov <kotaranov@microsoft.com>

drivers/infiniband/hw/mana/device.c
@@ -58,6 +58,7 @@ static int mana_ib_probe(struct auxiliary_device *adev,
 	struct net_device *upper_ndev;
 	struct mana_context *mc;
 	struct mana_ib_dev *dev;
+	u8 mac_addr[ETH_ALEN];
 	int ret;
 
 	mc = mdev->driver_data;
@@ -89,6 +90,7 @@ static int mana_ib_probe(struct auxiliary_device *adev,
 		ibdev_err(&dev->ib_dev, "Failed to get master netdev");
 		goto free_ib_device;
 	}
+	ether_addr_copy(mac_addr, upper_ndev->dev_addr);
 	ret = ib_device_set_netdev(&dev->ib_dev, upper_ndev, 1);
 	rcu_read_unlock();
 	if (ret) {
@@ -120,6 +122,13 @@ static int mana_ib_probe(struct auxiliary_device *adev,
 	ret = mana_ib_gd_create_rnic_adapter(dev);
 	if (ret)
 		goto destroy_eqs;
+
+	ret = mana_ib_gd_config_mac(dev, ADDR_OP_ADD, mac_addr);
+	if (ret) {
+		ibdev_err(&dev->ib_dev, "Failed to add Mac address, ret %d",
+			  ret);
+		goto destroy_rnic;
+	}
 
 	ret = ib_register_device(&dev->ib_dev, "mana_%d",
 				 mdev->gdma_context->dev);

dcui/linux-2.6.37-lis-backport

BugLink: https://bugs.launchpad.net/bugs/2125704
Link: https://lore.kernel.org/r/1712738551-22075-7-git-send-email-kotaranov@linux.microsoft.com
Signed-off-by: Leon Romanovsky <leon@kernel.org>
(cherry picked from commit 8859f009ace237ffc165c95edcc113d3824b9bf3)
Signed-off-by: John Cabaj <john.cabaj@canonical.com>
Acked-by: Vinicius Peixoto <vinicius.peixoto@canonical.com>
Acked-by: Philip Cox <philip.cox@canonical.com>
(cherry picked from commit 7547363b591357093fbe901efb40312af07e1de4 noble:linux-azure-nvidia)
Acked-by: Alessio Faina <alessio.faina@canonical.com>

drivers/infiniband/hw/mana/device.c
@@ -90,6 +90,7 @@ static int mana_ib_probe(struct auxiliary_device *adev,
 	struct net_device *ndev;
 	struct mana_context *mc;
 	struct mana_ib_dev *dev;
+	u8 mac_addr[ETH_ALEN];
 	int ret;
 
 	mc = mdev->driver_data;
@@ -120,6 +121,7 @@ static int mana_ib_probe(struct auxiliary_device *adev,
 		ibdev_err(&dev->ib_dev, "Failed to get netdev for IB port 1");
 		goto free_ib_device;
 	}
+	ether_addr_copy(mac_addr, ndev->dev_addr);
 	ret = ib_device_set_netdev(&dev->ib_dev, ndev, 1);
 	/* mana_get_primary_netdev() returns ndev with refcount held */
 	netdev_put(ndev, &dev->dev_tracker);
@@ -160,6 +162,13 @@ static int mana_ib_probe(struct auxiliary_device *adev,
 	ret = mana_ib_gd_create_rnic_adapter(dev);
 	if (ret)
 		goto destroy_eqs;
+
+	ret = mana_ib_gd_config_mac(dev, ADDR_OP_ADD, mac_addr);
+	if (ret) {
+		ibdev_err(&dev->ib_dev, "Failed to add Mac address, ret %d",
+			  ret);
+		goto destroy_rnic;
+	}
 
 	ret = ib_register_device(&dev->ib_dev, "mana_%d",
 				 mdev->gdma_context->dev);