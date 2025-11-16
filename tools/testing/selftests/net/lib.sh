53daf6abdc1b2f0dc7b86443ead88ba77438b394
Ido Schimmel <idosch@nvidia.com>
2025-11-16 10:10:29 +0200

    selftests: net: lib: Do not overwrite error messages
    BugLink: https://bugs.launchpad.net/bugs/2139624
    
    [ Upstream commit bed22c7b90af732978715a1789bca1c3cfa245a6 ]
    
    ret_set_ksft_status() calls ksft_status_merge() with the current return
    status and the last one. It treats a non-zero return code from
    ksft_status_merge() as an indication that the return status was
    overwritten by the last one and therefore overwrites the return message
    with the last one.
    
    Currently, ksft_status_merge() returns a non-zero return code even if
    the current return status and the last one are equal. This results in
    return messages being overwritten which is counter-productive since we
    are more interested in the first failure message and not the last one.
    
    Fix by changing ksft_status_merge() to only return a non-zero return
    code if the current return status was actually changed.
    
    Add a test case which checks that the first error message is not
    overwritten.
    
    Before:
    
     # ./lib_sh_test.sh
     [...]
     TEST: RET tfail2 tfail -> fail                                      [FAIL]
            retmsg=tfail expected tfail2
     [...]
     # echo $?
     1
    
    After:
    
     # ./lib_sh_test.sh
     [...]
     TEST: RET tfail2 tfail -> fail                                      [ OK ]
     [...]
     # echo $?
     0
    
    Fixes: 596c8819cb78 ("selftests: forwarding: Have RET track kselftest framework constants")
    Reviewed-by: Petr Machata <petrm@nvidia.com>
    Signed-off-by: Ido Schimmel <idosch@nvidia.com>
    Link: https://patch.msgid.link/20251116081029.69112-1-idosch@nvidia.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    [diewald: do not add test case to check new behavior since we're missing
    8ff2d7abfb6f ("selftests: forwarding: Add a test for testing lib.sh
    functionality")]
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Edoardo Canepa <edoardo.canepa@canonical.com>

--- tools/testing/selftests/net/lib.sh
+++ tools/testing/selftests/net/lib.sh
@@ -36,7 +36,7 @@ __ksft_status_merge()
 		weights[$i]=$((weight++))
 	done
 
-	if [[ ${weights[$a]} > ${weights[$b]} ]]; then
+	if [[ ${weights[$a]} -ge ${weights[$b]} ]]; then
 		echo "$a"
 		return 0
 	else
