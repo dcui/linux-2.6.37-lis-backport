intel-lab-lkp/linux

RDMA/mana_ib: set node_guid
Use the mac address for the node_guid of the IB device.

Signed-off-by: Konstantin Taranov <kotaranov@microsoft.com>
Reviewed-by: Long Li <longli@microsoft.com>

drivers/infiniband/hw/mana/device.c
@@ -5,6 +5,7 @@
 
 #include "mana_ib.h"
 #include <net/mana/mana_auxiliary.h>
+#include <net/addrconf.h>
 
 MODULE_DESCRIPTION("Microsoft Azure Network Adapter IB driver");
 MODULE_LICENSE("GPL");
@@ -92,6 +93,7 @@ static int mana_ib_probe(struct auxiliary_device *adev,
 		goto free_ib_device;
 	}
 	ether_addr_copy(mac_addr, upper_ndev->dev_addr);
+	addrconf_addr_eui48((u8 *)&dev->ib_dev.node_guid, upper_ndev->dev_addr);
 	ret = ib_device_set_netdev(&dev->ib_dev, upper_ndev, 1);
 	rcu_read_unlock();
 	if (ret) {

dcui/linux-2.6.37-lis-backport

BugLink: https://bugs.launchpad.net/bugs/2125704
Link: https://lore.kernel.org/r/1717070117-1234-2-git-send-email-kotaranov@linux.microsoft.com
Signed-off-by: Leon Romanovsky <leon@kernel.org>
(cherry picked from commit 65357e2c164a08bf20849dd55f46aa71e00334fa)
Signed-off-by: John Cabaj <john.cabaj@canonical.com>
Acked-by: Vinicius Peixoto <vinicius.peixoto@canonical.com>
Acked-by: Philip Cox <philip.cox@canonical.com>
(cherry picked from commit 775541b9adf2ea4b34728edf8e76816b00972ded noble:linux-azure-nvidia)
Acked-by: Alessio Faina <alessio.faina@canonical.com>

drivers/infiniband/hw/mana/device.c
@@ -5,6 +5,7 @@
 
 #include "mana_ib.h"
 #include <net/mana/mana_auxiliary.h>
+#include <net/addrconf.h>
 
 MODULE_DESCRIPTION("Microsoft Azure Network Adapter IB driver");
 MODULE_LICENSE("GPL");
@@ -122,6 +123,7 @@ static int mana_ib_probe(struct auxiliary_device *adev,
 		goto free_ib_device;
 	}
 	ether_addr_copy(mac_addr, ndev->dev_addr);
+	addrconf_addr_eui48((u8 *)&dev->ib_dev.node_guid, ndev->dev_addr);
 	ret = ib_device_set_netdev(&dev->ib_dev, ndev, 1);
 	/* mana_get_primary_netdev() returns ndev with refcount held */
 	netdev_put(ndev, &dev->dev_tracker);